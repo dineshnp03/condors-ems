ac4f715085e1a55aa12fa7147eb0af77a84f3c35
dineshnp03 <dineshnpcan@gmail.com>
2024-09-28 14:39:10 -0400

    first commit

---
+++ src/components/employee-directory.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class EmployeeDirectory extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Employee Directory</h1>
+            </div>
+        );
+    }
+}
+
+export default EmployeeDirectory;

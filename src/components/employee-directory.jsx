8439036ee6f97c1aac628c66188a5648cba92f67
Jemish Surani <93424969+jemishsurani06@users.noreply.github.com>
2024-10-13 13:29:15 -0400

    Update employee-directory.jsx

--- src/components/employee-directory.jsx
+++ src/components/employee-directory.jsx
@@ -1,14 +1,44 @@
 import { Component } from "react";
 import EmployeeTable from "./employee-table";
 import EmployeeCreate from "./employee-create";
+import EmployeeSearch from "./EmployeeSearch";
 
 class EmployeeDirectory extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [], 
+      filteredEmployees: [],  
+    };
+  }
+
+   addEmployee = (newEmployee) => {
+    const updatedEmployees = [...this.state.employees, newEmployee];
+    this.setState({
+      employees: updatedEmployees,
+      filteredEmployees: updatedEmployees,
+    });
+  };
+
+  searchEmployees = (filters) => {
+    const filtered = this.state.employees.filter((emp) => {
+      return (
+        (filters.age ? emp.age === parseInt(filters.age) : true) &&
+        (filters.department ? emp.department === filters.department : true) &&
+        (filters.title ? emp.title === filters.title : true)
+      );
+    });
+    this.setState({ filteredEmployees: filtered });
+  };
+  
   render() {
     return (
       <div>
         <h1>Employee Directory</h1>
         <EmployeeTable />
         <EmployeeCreate />
+        <EmployeeSearch />
       </div>
     );
   }

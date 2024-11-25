6dd66cbf4872f84445e56459b58ce01b113122bc
dineshnp03 <dineshnpcan@gmail.com>
2024-11-25 03:20:25 -0500

    worked on the Delete Modal and condition on Active Emp delete

--- src/components/employee-table.jsx
+++ src/components/employee-table.jsx
@@ -1,25 +1,87 @@
 import { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 class EmployeeTable extends Component {
-  handleDelete(employee) {
-    if (
-      window.confirm(`
-      Are you sure you want to delete ${employee.lastName}, ${employee.firstName} from the list?
-      `)
-    ) {
-      this.props.deleteEmployee(employee.id);
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      employee: null,
+      alert: {
+        show: false,
+        message: "",
+        variant: "",
+      },
+    };
+  }
+
+  handleClose = () => {
+    this.setState({
+      show: false,
+    });
+  };
+
+  handleDelete = (employee) => {
+    if (employee.currentStatus) {
+      this.setState({
+        alert: {
+          show: true,
+          message:
+            "Employee is currently working, Cannot delete a working Employee",
+          variant: "danger",
+        },
+      });
+    } else {
+      this.setState(
+        {
+          show: true,
+          employee,
+        },
+        () => {
+          console.log(this.state.employee);
+        }
+      );
+    }
+  };
+
+  confirmDelete = () => {
+    this.props.deleteEmployee(this.state.employee.id);
+    this.setState({
+      show: false,
+    });
+  };
+
+  componentDidUpdate() {
+    if (this.state.alert.show) {
+      setTimeout(() => {
+        this.setState({
+          alert: {
+            show: false,
+          },
+        });
+      }, 3000);
     }
   }
+
   render() {
     const { employees } = this.props;
 
     return (
-      <div>
-        <h3 className="mb-3">Employee Directory</h3>
-        <div className="table-responsive">
-          <table className="table table-bordered table-striped">
+      <>
+        {this.state.alert.show ? (
+          <div
+            className={`alert alert-${this.state.alert.variant}`}
+            role="alert"
+          >
+            <strong>Warning!</strong> {this.state.alert.message}
+          </div>
+        ) : (
+          ""
+        )}
+        <div>
+          <h3 className="mb-3">Employee Directory</h3>
+          <Table responsive striped bordered condensed hover>
             <thead className="table-dark">
               <tr>
                 <th>First Name</th>
@@ -75,7 +137,6 @@ class EmployeeTable extends Component {
                       >
                         Delete
                       </Button>{" "}
-                       
                     </td>
                   </tr>
                 ))
@@ -87,9 +148,37 @@ class EmployeeTable extends Component {
                 </tr>
               )}
             </tbody>
-          </table>
+          </Table>
         </div>
-      </div>
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={this.state.show}
+          onHide={this.handleClose}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Are you Sure?
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Are you sure you wanna delete this Employee</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              className="me-4"
+              variant="danger"
+              onClick={this.confirmDelete}
+            >
+              Confirm
+            </Button>
+            <Button variant="default" onClick={this.handleClose}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   }
 }
